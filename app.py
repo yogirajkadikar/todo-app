c5f17615d7076e45df255b11e8e1d67242f77a41
Yogiraj <yogiraj.kadikar@gmail.com>
2020-04-22 17:53:58 +0530

    delete function added

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ class Todo(db.Model):
     text= db.Column(db.String(250))
     complete= db.Column(db.Boolean)
 
-
-    
-
-
 @app.route('/')
 def index():
     incomplete =Todo.query.filter_by(complete= False).all()
@@ -35,8 +31,18 @@ def complete(id):
     todo=Todo.query.filter_by(id=id).first()
     todo.complete = True
     db.session.commit()
-    
     return redirect(url_for('index'))
 
+
+@app.route('/delete/<id>')
+def delete(id):
+    todo = Todo.query.get(id)
+    if not todo:
+        return redirect(url_for('index'))
+
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+    
 if(__name__) == "__main__":
     app.run(debug=True)
